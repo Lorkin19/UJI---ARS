f6e26d072203c886c37280c2c120f12302aca280
GuillermoAlaejos <guillermo.alaejos@gmail.com>
2019-11-17 14:53:45 +0100

    Interfaz de profesor

--- UJIARS/src/users/Profesor.java
+++ UJIARS/src/users/Profesor.java
@@ -1,5 +1,25 @@
 package users;
 
-public class Profesor {
+import server.Pregunta;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Profesor extends UnicastRemoteObject implements IProyector {
     private IProyector proyector;
+    private String nombre;
+    private String password;
+    
+    public Profesor() throws RemoteException {
+        super();
+    }
+    @Override
+    public void verPregunta(Pregunta pregunta) throws RemoteException {
+
+    }
+
+    @Override
+    public void verResultados(Pregunta pregunta) throws RemoteException {
+
+    }
 }
